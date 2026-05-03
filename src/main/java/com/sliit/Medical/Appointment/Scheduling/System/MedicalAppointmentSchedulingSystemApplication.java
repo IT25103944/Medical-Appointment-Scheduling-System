048a164e302f4f5c0e6d486a1826f124271b56a8
sahanirandaka <contact.sahanirandaka@gmail.com>
2026-05-03 12:33:13 +0530

    first commit

---
+++ src/main/java/com/sliit/Medical/Appointment/Scheduling/System/MedicalAppointmentSchedulingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.sliit.Medical.Appointment.Scheduling.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentSchedulingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sliit/Medical/Appointment/Scheduling/System/MedicalAppointmentSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.Medical.Appointment.Scheduling.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
